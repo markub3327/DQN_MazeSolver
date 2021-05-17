75c0d025321fe289dc6fd5aaa9ff5c4bdf6c5627
Martin Kubovčík <74611856+markub3327@users.noreply.github.com>
2021-05-17 16:12:58 +0200

    Update agent.py

--- agent.py
+++ agent.py
@@ -2,28 +2,9 @@ import tensorflow as tf
 import numpy as np
 
 from tensorflow.keras import Model
-from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.layers import Input
 from tensorflow.keras.utils import plot_model
 from tensorflow_addons.layers.noisy_dense import NoisyDense
-
-class AgentModel(Model):
-
-    def __init__(self, hidden, action_dim):
-        super(AgentModel, self).__init__()
-        
-        self.hidden_layer = []
-        for i in range(len(hidden)):
-            self.hidden_layer.append(NoisyDense(hidden[i], activation='relu', kernel_initializer='he_uniform'))
-
-        # vystupna vrstva   -- musi byt linear ako posledna vrstva pre regresiu Q funkcie (-nekonecno, nekonecno)!!!
-        self.q_values = Dense(action_dim, activation='linear', kernel_initializer='glorot_uniform')
-
-    def call(self, inputs, reset_noise, remove_noise):
-        x = self.hidden_layer[0](inputs)
-        for i in range(1, len(self.hidden_layer)):
-            x = self.hidden_layer[i](x, reset_noise=reset_noise, remove_noise=remove_noise)
-
-        return self.q_values(x)
 
 class Agent:
     def __init__(self, state_dim=None, action_dim=None, hidden=None, lr=0.001, fileName=None):
@@ -34,46 +15,74 @@ class Agent:
 
     def create_network(self, state_dim, action_dim, hidden, fileName, lr):
         if (fileName == None):
-            model = AgentModel(hidden, action_dim)
+            # vstupna vsrtva pre state
+            state_input = Input(shape=state_dim)
+
+            l = state_input
+            for i in range(len(hidden)):
+                l = NoisyDense(hidden[i], activation='relu', kernel_initializer='he_uniform')(l)
+
+            # vystupna vrstva   -- musi byt linear ako posledna vrstva pre regresiu Q funkcie (-nekonecno, nekonecno)!!!
+            output = NoisyDense(action_dim, activation='linear', kernel_initializer='glorot_uniform')(l)
+
+            # Vytvor model
+            model = Model(inputs=state_input, outputs=output)
+
+            # Skompiluj model
+            model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr), loss='mse')
+
+            model.summary()
 
             print("Created successful")
         else:
             model = tf.keras.models.load_model(fileName, compile=False)
 
+            # Skompiluj model
+            model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr), loss='mse')
+
+            #model.summary()
+
             print("Loaded successful")
 
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
-        self.loss_fn = tf.keras.losses.MeanSquaredError()
-    
         return model
+    
+    @tf.function
+    def predict(self, state):
+        return tf.squeeze(self.model(tf.expand_dims(state, axis=0)), axis=0)            # remove batch_size dim
+    
+    def reset_noise_target(self):
+        for l in self.target_model.layers[1:]:
+            l.reset_noise()
 
-    #@tf.function
-    def predict(self, state, reset_noise, remove_noise):
-        return tf.squeeze(self.model(tf.expand_dims(state, axis=0), reset_noise=reset_noise, remove_noise=remove_noise), axis=0)            # remove batch_size dim
+    def reset_noise(self):
+        for l in self.model.layers[1:]:
+            l.reset_noise()
+
+    def remove_noise(self):
+        for l in self.model.layers[1:]:
+            l.remove_noise()
 
     def train(self, replay_buffer, batch_size, gamma, tau):
         states, actions, rewards, next_states, dones = replay_buffer.sample(batch_size)
 
         # predikuj akcie pre stavy            
-        Q_targets = self.model(states, reset_noise=False, remove_noise=False).numpy()
+        targets = self.model(states).numpy()
+        #print(targets, targets.shape)
+
+        # reset Q target net's noise params
+        self.reset_noise_target()
 
         # predikuj buduce akcie podla target siete
-        Q_futures = self.target_model(next_states, reset_noise=True, remove_noise=False)
+        Q_futures = self.target_model(next_states)
         Q_futures = tf.reduce_max(Q_futures, axis=1)
         #print(Q_futures, Q_futures.shape)
 
         # vypocitaj TD error
-        Q_targets[(np.arange(batch_size), actions)] = rewards + ((1-dones) * gamma * Q_futures)
+        targets[(np.arange(batch_size), actions)] = rewards + ((1-dones) * gamma * Q_futures)
+        #print(targets, targets.shape)
 
         # pretrenuj model
-        with tf.GradientTape() as tape:
-            q_vals = self.model(states, reset_noise=False, remove_noise=False)
-            loss = self.loss_fn(
-                y_true=Q_targets, y_pred=q_vals
-            )
-
-        grads = tape.gradient(loss, self.model.trainable_weights)
-        self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
+        loss = self.model.train_on_batch(states, targets)
 
         # pretrenuj target siet
         self._update_target(self.model, self.target_model, tau=tf.constant(tau))
